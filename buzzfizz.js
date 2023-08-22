75a2cec3841bfe2410770359154e2ff701ac10f3
Alan <alanechabarri@gmail.com>
2023-08-22 15:47:45 -0300

    primer ejercicio de javaScript resuelto

--- buzzfizz.js
+++ buzzfizz.js
@@ -4,6 +4,7 @@
  *  sino es ninguno devuelve la misma entrada.
  * **/
 function buzzfizz(numero) {
+    if (!numero) { return numero };
     var ret = "";
     if (numero % 3 == 0) {
         ret = "Fizz";
@@ -15,6 +16,17 @@ function buzzfizz(numero) {
 
     return ret || numero;// evaluar dos valores y devolver el primer valor verdadero que encuentra
 
+}
+
+//solucion del profe
+const multiplos = { 3: 'fizz', 5: 'buzz' }
+
+function fizzbuzz(num) {
+    let palabra = '';
+    Object.entries(multiplos).forEach(([indice, valor]) => {
+        if (!(num % indice)) palabra += valor;
+    })
+    return palabra || num;
 }
 const argumento = process.argv[2];// input del usuario
 const numero = parseInt(argumento);// no se si es necesario, porque es javaScript xd
